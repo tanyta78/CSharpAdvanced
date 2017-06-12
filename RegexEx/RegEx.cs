7bd4e75ac2ee15051b3a9c69847c7e31b5d1e5d2
tanyta78 <tanyta78@gmail.com>
2017-06-12 21:12:29 +0300

    RegexEx
    RegexExers

--- RegexEx/RegEx.cs
+++ RegexEx/RegEx.cs
@@ -2,14 +2,230 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace RegexEx
 {
-   public class RegEx
+    public class RegEx
     {
         static void Main()
         {
+
+
+            Regex openingTagRegex = new Regex(@"<div(.*?\b)((id|class)\s*=\s*""(.*?)"")(.*?)>");
+            Regex closingTagRegex = new Regex(@"<\/div>\s*<!--\s*(.*?)\s*-->");
+            string text = Console.ReadLine();
+
+
+            while (text != null && !text.Equals("END"))
+            {
+                var openingTagMatch = openingTagRegex.Match(text);
+                var closingTagMatch = closingTagRegex.Match(text);
+                if (openingTagMatch.Success)
+                {
+
+                    text = $"{openingTagMatch.Groups[4].Value} {openingTagMatch.Groups[1].Value} ";
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(text);
+                    for (int i = 5; i <= openingTagMatch.Groups.Count; i++)
+                    {
+                        var currentInfo = openingTagMatch.Groups[i].Value+" ";
+                        sb.Append(currentInfo);
+                    }
+                    text = sb.ToString().Trim();
+                    text = "<" + Regex.Replace(text, @"\s+", " ") + ">";
+                }
+
+                if (closingTagMatch.Success)
+                {
+                    text = $"</{closingTagMatch.Groups[1].Value}>";
+                }
+
+                Console.WriteLine(text);
+                text = Console.ReadLine();
+            }
+
+        }
+
+        public static void SemanticHtml()
+        {
+
+        }
+
+        public static void ExtractHiperlinks()
+        {
+            string pattern = @"<a[\s\S]*?\s+href\s*={0,1}\s*([""'])?([\s\S]*?)(?:>|class|\1)[\s\S]*?<\/a>";
+
+            Regex newRegex = new Regex(pattern);
+            string text = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+
+            while (text != null && !text.Equals("END"))
+            {
+                sb.Append(text);
+                text = Console.ReadLine();
+            }
+
+            string allText = sb.ToString();
+
+            if (newRegex.IsMatch(allText))
+            {
+                var matches = newRegex.Matches(allText);
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Groups[2].Value);
+                }
+
+            }
+        }
+
+        public static void ValidUserNames()
+        {
+            string pattern = "(?<=[\\s\\/\\\\(\\)]|^)([A-Za-z]\\w{2,24})(?=[\\s\\/\\\\(\\)]|$)";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+            var userNamesMaxLength = int.MinValue;
+            var bestIndex = -1;
+            if (newRegex.IsMatch(text))
+            {
+                var matches = newRegex.Matches(text);
+
+                var prevMatch = matches[0].Value.Length;
+                for (int i = 1; i < matches.Count; i++)
+                {
+                    var currentMatch = matches[i].Value.Length;
+                    if (currentMatch + prevMatch > userNamesMaxLength)
+                    {
+                        userNamesMaxLength = currentMatch + prevMatch;
+                        bestIndex = i;
+                    }
+                    prevMatch = currentMatch;
+                }
+
+                Console.WriteLine(matches[bestIndex - 1].Value);
+                Console.WriteLine(matches[bestIndex].Value);
+            }
+        }
+
+        public static void SentenceExtractor()
+        {
+            string pattern = "([^.!?]+(?=[.!?])[.!?])";
+
+            Regex isSentenceRegex = new Regex(pattern);
+
+            string searchString = " " + Console.ReadLine() + " ";
+            string text = Console.ReadLine();
+
+            Regex isContained = new Regex(searchString);
+
+            if (isSentenceRegex.IsMatch(text))
+            {
+                var matches = isSentenceRegex.Matches(text);
+                foreach (Match match in matches)
+                {
+                    var isTrue = isContained.IsMatch(match.Value);
+                    if (isTrue)
+                    {
+                        Console.WriteLine(match.Value);
+                    }
+
+                }
+            }
+        }
+
+        public static void ExtractEmails()
+        {
+            string pattern = "((?<=\\s)[a-zA-Z0-9]+([-.]\\w*)*@[a-zA-Z]+?([.-][a-zA-Z]*)*(\\.[a-z]{2,}))";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+
+            if (newRegex.IsMatch(text))
+            {
+                var matches = newRegex.Matches(text);
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+            }
+        }
+
+        public static void ReplaceTags()
+        {
+            string pattern = "(<a)(.*)(>+)(.*)(<\\/a>)";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            while (text != null && !text.Equals("end"))
+            {
+                if (newRegex.IsMatch(text))
+                {
+                    var matches = newRegex.Matches(text);
+                    Console.WriteLine(Regex.Replace(text, pattern,
+                        "[URL" + matches[0].Groups[2] + "]" + matches[0].Groups[4] + "[/URL]"));
+                }
+                else
+                {
+                    Console.WriteLine(text);
+                }
+                text = Console.ReadLine();
+            }
+        }
+
+        public static void SeriesOfLetter()
+        {
+            string pattern = "([a-z,A-Z])\\1+";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            Console.WriteLine(Regex.Replace(text, pattern, "$1"));
+        }
+
+        public static void PhoneNumber()
+        {
+            string pattern = "\\+359( |-)([0-9])\\1([0-9]{3})\\1([0-9]{4})\\b";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            while (text != null && !text.Equals("end"))
+            {
+                if (newRegex.IsMatch(text))
+                {
+                    Console.WriteLine(text);
+                }
+                text = Console.ReadLine();
+            }
+
+        }
+
+        public static void FullName()
+        {
+            string pattern = "\\b([A-Z][a-z]+) ([A-Z][a-z]+)\\b";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            while (text != null && !text.Equals("end"))
+            {
+                if (newRegex.IsMatch(text))
+                {
+                    Console.WriteLine(text);
+                }
+                text = Console.ReadLine();
+            }
+
         }
     }
 }
---
+++ RegexLab/RegexLab.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace RegexLab
+{
+    public class RegexLab
+    {
+        public static void Main()
+        {
+
+            string pattern = "(\"|')(.*?)\\1";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(text, pattern);
+
+            foreach (Match match in matches)
+            {
+               Console.WriteLine(match.Groups[2].Value);
+            }
+
+        }
+
+        public static void ExtractQuatiations()
+        {
+            
+        }
+
+        public static void ValidTime()
+        {
+            string pattern = @"^([01][0-9]):([0-5][0-9]):([0-5][0-9]) [AP]M$";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            while (text != null && !text.Equals("END"))
+            {
+                Console.WriteLine(newRegex.IsMatch(text) ? "valid" : "invalid");
+                text = Console.ReadLine();
+            }
+        }
+
+        public static void ValidNames()
+        {
+
+            string pattern = @"^[\w\d-]{3,16}$";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            while (text != null && !text.Equals("END"))
+            {
+                Console.WriteLine(newRegex.IsMatch(text) ? "valid" : "invalid");
+                text = Console.ReadLine();
+            }
+        }
+
+        public static void ExtractTags()
+        {
+            string pattern = @"<[^>]*>";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            while (text != null && !text.Equals("END"))
+            {
+                while (newRegex.IsMatch(text))
+                {
+                    var result = newRegex.Match(text);
+                    Console.WriteLine(result.Value);
+                    text = text.Substring(result.Index + result.Value.Length);
+                }
+                text = Console.ReadLine();
+            }
+        }
+
+        public static void ExtractIntegerNumbers()
+        {
+            string pattern = @"\d+";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            if (text != null)
+            {
+                while (newRegex.IsMatch(text))
+                {
+                    var result = newRegex.Match(text);
+                    Console.WriteLine(result.Value);
+                    text = text.Substring(result.Index + result.Value.Length);
+                }
+            }
+        }
+
+        public static void NonDigitCount()
+        {
+            string pattern = "[\\D]";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            if (text != null)
+            {
+                int result = newRegex.Matches(text).Count;
+
+                Console.WriteLine($"Non-digits: {result}");
+            }
+        }
+
+        public static void VowelCount()
+        {
+            string pattern = "[aAeEiIuUyYoO]";
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            if (text != null)
+            {
+                int result = newRegex.Matches(text).Count;
+
+                Console.WriteLine($"Vowels: {result}");
+            }
+        }
+
+        public static void MatchCount()
+        {
+            string pattern = Console.ReadLine();
+
+            Regex newRegex = new Regex(pattern);
+
+            string text = Console.ReadLine();
+
+            if (text != null)
+            {
+                int result = newRegex.Matches(text).Count;
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
